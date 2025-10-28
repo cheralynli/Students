4a8c3c3f7d82550a838bf48d3a1d29c332d2c964
cheralynli <cheralynlianto@gmail.com>
2025-10-29 00:14:19 +0800

    Final report

--- Students.py
+++ Students.py
@@ -16,7 +16,7 @@ def read_input_data():
         grade_records.append({"student id": std_id, "subject": subject, "score": grade})
     return students_db, grade_records
 
-def calculate_subject_metrics(grade_records)
+def calculate_subject_metrics(grade_records):
     subject_stats={}
     for record in grade_records:
         subject = record["subject"]
@@ -59,5 +59,37 @@ def calculate_student_total_scores(students_db, grade_records):
         student_scores.append({
             "id": std_id,
             "name": std_name,
-            "total score": total_score
+            "total_score": total_score
         })
+    return student_scores
+
+def generate_formatted_report(students_db, grade_records, subject_stats, student_scores):
+    total_students= len(students_db)
+    total_subjects= len(subject_stats)
+    avg_subject_scores= sum(record["score"] for record in grade_records)/len(grade_records)
+
+    print(f"Total Students: {total_students}"
+          f"\nTotal Subject: {total_subjects}"
+          f"\nAverage Score: {avg_subject_scores:.1f}\n")
+    
+    print("Subject Analysis:")
+    for subject, stats in sorted(subject_stats.items()):
+        sub_name= subject.ljust(10)
+        print(f"{sub_name}: Avg: {stats['avg']:.1f}, Max: {stats['max']:.1f}, Min: {stats['min']:.1f}, Pass Rate: {stats['pass_rate']*100:.1f}%")
+    print()
+    print("Student Top-3 Rankings:")
+    print("Student ID".ljust(13) + "Student Name".ljust(15) + "Total Score")
+    for student in sorted(student_scores, key=lambda x: x["total_score"], reverse=True)[:3]:
+        student_id= student["id"].ljust(13)
+        student_name= student["name"].ljust(15)
+        total_score= f"{student['total_score']:.1f}"
+        print(f"{student_id}{student_name}{total_score}")
+
+def main():
+    students_db, grade_records = read_input_data()
+    subject_stats = calculate_subject_metrics(grade_records)
+    student_scores = calculate_student_total_scores(students_db, grade_records)
+    generate_formatted_report(students_db, grade_records, subject_stats, student_scores)
+
+if __name__ == "__main__":
+    main()
